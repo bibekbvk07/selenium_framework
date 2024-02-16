852ed9f404480edafe768efec9918a893b3eec89
bibekbvk07 <bibekamatya7@gmail.com>
2024-02-16 03:49:35 -0600

    modified

--- src/test/java/org/example/testngtests/TestLogin.java
+++ src/test/java/org/example/testngtests/TestLogin.java
@@ -75,27 +75,6 @@ public class TestLogin {
         driver.findElement(By.xpath(xpath)).click();
     }
 
-    @Test(dependsOnMethods = "LoginTest")
-    public void test3(){
-        for (int i = 1; i <=6; i++){
-            String product_name = driver.findElement(By.xpath("(//div[@class='inventory_list']//div[@class='inventory_item'])["+i+"]//div[@class='inventory_item_name ']" )).getText();
-            String item_price = driver.findElement(By.xpath("(//div[@class='inventory_list']//div[@class='inventory_item'])["+i+"]//div[@class='inventory_item_price']")).getText();
-
-            driver.findElement(By.xpath("(//div[@class='inventory_container']//div[@class='inventory_item'])["+i+"]//div[@class='pricebar']//button ")).click();
-
-            System.out.println("Product Name: "+ product_name+"\t"+ "Item_Price: "+ item_price+"\n");
-        }
-        int cart_Items = Integer.parseInt(driver.findElement(By.className("shopping_cart_badge")).getText());
-        System.out.println(cart_Items);
-
-        Assert.assertEquals(cart_Items,6);
-
-        driver.findElement(By.className("shopping_cart_link")).click();
-
-        List<WebElement> list = driver.findElements(By.xpath("(//div[@class='cart_list']//div[@class='cart_item'])"));
-
-
-    }
 
     @Test(dependsOnMethods = "LoginTest")
     public void test2(){
@@ -137,6 +116,91 @@ public class TestLogin {
                 Assert.assertEquals(item_price, expected_price, "Actual & expected price don't match");
             }
         }
+    }
+
+    @Test(dependsOnMethods = "LoginTest")
+    public void test3(){
+        Map<String, String> itemCollections = new HashMap<>();
+        for (int i = 1; i <=6; i++){
+            String product_name = driver.findElement(By.xpath("(//div[@class='inventory_list']//div[@class='inventory_item'])["+i+"]//div[@class='inventory_item_name ']" )).getText();
+            String item_price = driver.findElement(By.xpath("(//div[@class='inventory_list']//div[@class='inventory_item'])["+i+"]//div[@class='inventory_item_price']")).getText();
+
+            itemCollections.put(product_name, item_price);
+
+            driver.findElement(By.xpath("(//div[@class='inventory_container']//div[@class='inventory_item'])["+i+"]//div[@class='pricebar']//button ")).click();
+        }
+        // Print the HashMap
+        System.out.println("HashMap contents: ");
+        for (Map.Entry<String, String> entry: itemCollections.entrySet()){
+            String productName = entry.getKey();
+            String price = entry.getValue();
+
+            System.out.println(productName + ": "+ price);
+        }
+        int cart_Items = Integer.parseInt(driver.findElement(By.className("shopping_cart_badge")).getText());
+        System.out.println(cart_Items);
+
+        Assert.assertEquals(cart_Items,6);
+
+        driver.findElement(By.xpath("//div[@id='shopping_cart_container']/a[@class='shopping_cart_link']")).click();
+
+//      driver.findElement(By.className("shopping_cart_link")).click();
+
+        List<WebElement> itemList = driver.findElements(By.xpath("(//div[@class='cart_list']//div[@class='cart_item'])"));
+
+        Assert.assertEquals(itemList.size(), 6);
+
+        for (int i = 1; i <= itemList.size(); i++){
+            String product_name = driver.findElement(By.xpath("(//div[@class='cart_item']//div[@class='inventory_item_name'])["+i+"]")).getText();
+            String product_price = driver.findElement(By.xpath("(//div[@class='cart_item']//div[@class='inventory_item_price'])["+i+"]")).getText();
+
+            String expectedPrice = itemCollections.get(product_name);
+
+            if (expectedPrice == null){
+                Assert.fail("Not Present!");
+            }else{
+                Assert.assertEquals(product_price, expectedPrice,"Actual and Expected Price do not match");
+            }
+        }
+
+        driver.findElement(By.xpath("//div[@class='cart_footer']//button[@id='checkout']")).click();
+
+        driver.findElement(By.id("first-name")).sendKeys("Bibek");
+        driver.findElement(By.id("last-name")).sendKeys("Shrestha");
+        driver.findElement(By.id("postal-code")).sendKeys("56001");
+
+        driver.findElement(By.id("continue")).click();
+
+        List<WebElement> checkoutList = driver.findElements(By.xpath("(//div[@id='checkout_summary_container']//div[@class='cart_list']//div[@class='cart_item'])"));
+        Assert.assertEquals(checkoutList.size(), 6);
+
+        double checkoutTotal = 0.0;
+
+        for (int i = 1; i <= checkoutList.size(); i++){
+            String product = driver.findElement(By.xpath("(//div[@class='cart_item']//div[@class='inventory_item_name'])["+i+"]")).getText();
+            String price = driver.findElement(By.xpath("(//div[@class='cart_item']//div[@class='item_pricebar'])["+i+"]")).getText();
+
+            if (itemCollections.get(product) == null){
+                Assert.fail("Not Present!");
+            }else {
+                Assert.assertEquals(price, itemCollections.get(product), "Actual and Expected Price do not match");
+            }
+
+            checkoutTotal += Double.parseDouble(price.substring(1));
+        }
+
+        // Total Price Validation
+        double itemTotal = Double.parseDouble((driver.findElement(By.className("summary_subtotal_label")).getText()).replaceAll("[^\\d.]",""));
+
+        Assert.assertEquals(checkoutTotal, itemTotal, "Price doesn't match!");
+
+        // Total tax
+        double totalTax = Double.parseDouble((driver.findElement(By.className("summary_tax_label")).getText()).replaceAll("[^\\d.]",""));
+
+        double Total = Double.parseDouble((driver.findElement(By.className("summary_total_label")).getText()).replaceAll("[^\\d.]",""));
+
+        Assert.assertEquals((itemTotal+totalTax), Total, "Total Price doesn't match");
+
     }
 
 //    @AfterTest
